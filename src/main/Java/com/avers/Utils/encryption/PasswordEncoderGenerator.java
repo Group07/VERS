fd662efeac52f58d3103e3ad0f9edc29797ba298
Amila Amunugama <amila.efac@gmail.com>
2015-07-10 01:08:03 +0530

    util class added

---
+++ src/main/Java/com/avers/Utils/encryption/PasswordEncoderGenerator.java
@@ -0,0 +1,20 @@
+package com.avers.Utils.encryption;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by Amila on 7/10/2015.
+ */
+public class PasswordEncoderGenerator {
+
+    public String bCrypt(String password, int iterations){
+        int i = 0;
+        String hashedPassword = "";
+        while (i<iterations){
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            hashedPassword = passwordEncoder.encode(password);
+            i++;
+        }
+        return hashedPassword;
+    }
+}
