29143bdcfd7d27517bb521ecacc6618f3acbc484
Amila Amunugama <amila.efac@gmail.com>
2015-07-15 11:16:05 +0530

    controller created for add subject

--- src/main/Java/com/avers/controllers/LecturerHomeController.java
+++ src/main/Java/com/avers/controllers/LecturerHomeController.java
@@ -1,9 +1,12 @@
 package com.avers.controllers;
 
 import com.avers.Utils.audit.LogWrapper;
+import com.avers.dto.UserDTO;
+import com.avers.dto.UserRolesDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -21,6 +24,38 @@ public class LecturerHomeController {
         //testing logging Wrapper functionality
         LogWrapper logWrapper = new LogWrapper(LecturerHomeController.class);
         logWrapper.info("lecturer Logged");
+
+        return model;
+
+    }
+
+    @RequestMapping(value = "/addLecturer", method = RequestMethod.POST)
+    public ModelAndView addLecturer(@RequestParam String subjectcode,
+                                    @RequestParam String subjectname,
+                                    @RequestParam String semester) {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("lecturer");
+
+        LogWrapper logWrapper = new LogWrapper(AdminHomeController.class);
+        logWrapper.info("request to create subject: subjectcode = " + subjectcode + " & subjectname = " + subjectname + " & semester = " + semester);
+
+        if (subjectcode != null && !subjectcode.trim().isEmpty() & subjectname != null && !subjectname.trim().isEmpty() & semester != null && !semester.trim().isEmpty()) {
+
+            //TODO complete this
+/*            //insert data into user tables;
+            UserDTO user = new UserDTO(username, password);
+            userService.insertUser(user);
+
+            //insert data into user_roles;
+            UserRolesDTO userRoles = new UserRolesDTO("ROLE_LECTURER", username);
+            userService.insertUserRole(userRoles);*/
+
+            model.addObject("message", "Submitted");
+
+        } else {
+            model.addObject("message", "Error With submitted parameters");
+        }
 
         return model;
 
